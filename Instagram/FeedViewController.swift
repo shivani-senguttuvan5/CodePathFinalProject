4126548047a9b1e4cf53cb6b598ee39dea517220
Giang Tran <quynhgiangg124@gmail.com>
2022-11-04 22:34:51 -0500

    update like and unlike features

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -131,7 +131,6 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             let user = post["author"] as! PFUser
             
             cell.usernameLabel.text = user.username
-            
             cell.captionLabel.text = post["caption"] as! String
             
             let imageFile = post["image"] as! PFFileObject
@@ -139,6 +138,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             let url = URL(string: urlString)!
             
             cell.photoView.af.setImage(withURL: url)
+            cell.numLikes.text = post["likeCount"] as? String //show current number of likes
             
             return cell
         
@@ -162,8 +162,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let post = posts[indexPath.section]
         //comment object
-        let comments = (post["comments"] as? [PFObject]) ?? [] //makes a new comment table after saving an object like that
-        
+        let comments = (post["comments"] as? [PFObject]) ?? []; //makes a new comment table after saving an object like that
         if indexPath.row == comments.count + 1 {
             showsCommentBar = true
             becomeFirstResponder()
--- Instagram/PostCell.swift
+++ Instagram/PostCell.swift
@@ -6,11 +6,62 @@
 //
 
 import UIKit
+import Parse
 
 class PostCell: UITableViewCell {
+    var liked:Bool = false
+    var unliked:Bool = false
+    
     @IBOutlet weak var photoView: UIImageView!
     @IBOutlet weak var captionLabel: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var unlikeButton: UIButton!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var numLikes: UILabel!
+    
+    func setLike(_ isLiked: Bool) {
+        liked = isLiked
+        if (liked) {
+            likeButton.setImage(UIImage(named: "hand.thumbsup.fill"), for: UIControl.State.normal)
+            unlikeButton.setImage(UIImage(named: "hand.thumbsdown"), for: UIControl.State.normal)
+        }
+        else {
+            likeButton.setImage(UIImage(named: "hand.thumbsup"), for: UIControl.State.normal)
+        }
+    }
+    
+    func setUnlike(_ isUnliked: Bool) {
+        unliked = isUnliked
+        if (unliked) {
+            unlikeButton.setImage(UIImage(named: "hand.thumbsdown.fill"), for: UIControl.State.normal)
+            likeButton.setImage(UIImage(named: "hand.thumbsup"), for: UIControl.State.normal)
+        }
+        else {
+            unlikeButton.setImage(UIImage(named: "hand.thumbsdown"), for: UIControl.State.normal)
+        }
+    }
+    
+    @IBAction func likedPost(_ sender: Any) {
+        let tobeLiked = !liked
+        if (tobeLiked) {
+            self.setLike(true)
+        }
+        else {
+            self.setLike(false)
+        }
+    }
+    
+    @IBAction func unlikedPost(_ sender: Any) {
+    
+        let tobeUnliked = !unliked
+        if (tobeUnliked) {
+            self.setUnlike(true)
+        }
+        else {
+            self.setUnlike(false)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
